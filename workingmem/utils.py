c8e5bf18fcd5ec528a8134feb27589242f64600b
aalok <aalok@brown.edu>
2026-03-09 23:12:18 -0400

    write function to download runs from w&b based on sweep configs output in experiment directories

--- workingmem/utils.py
+++ workingmem/utils.py
@@ -1,11 +1,17 @@
+#!/bin/bash
+from pandas.core.frame import DataFrame
+
 from itertools import product
 import typing
 from functools import lru_cache
 import logging
+from pathlib import Path
 
+import yaml
 import pandas as pd
 from tqdm.auto import tqdm
 import wandb
+import dacite
 
 wandbapi = wandb.Api()
 
@@ -38,24 +44,95 @@ def print_gpu_mem(obj: typing.Any = None):
 
 
 @lru_cache(maxsize=None)
-def get_wandb_runs(
-    project_name="wm-comp-limit-7.3.1", sweep_id="4rrckcd1", user="aloxatel"
-):
-    runs = wandbapi.sweep(f"{user}/{project_name}/{sweep_id}").runs
+def _get_wandb_runs(
+    project_name: str, sweep_id: str, prefix=wandbapi.viewer.username, samples=20_000
+) -> DataFrame:
+    """
+    for a given workspace/user prefix and sweep ID retrieves all the runs corresponding
+    to that sweep. in this experimental framework each sweep should represent a single
+    experimental condition. so every sweep is associated with identical metadata save for
+    random seed in cases where models are randomly initialized, and the random shuffling
+    of datapoints during runs. this code, however, only includes so many hyperparameters
+    as were included in the config sent to wandb to initialize the run. does not extensively
+    catalog all the default parameters
+    """
+    from workingmem import MainConfig, ModelConfig, TrainingConfig, SIRConfig
+
+    runs = wandbapi.sweep(f"{prefix}/{project_name}/{sweep_id}").runs
     dfs = []
     for run in tqdm([*runs]):
-        metrics = run.history(pandas=True, samples=10_000)
+        metrics: pd.DataFrame = run.history(pandas=True, samples=samples)
         metrics["run_id"] = run.name
         metrics["sweep_id"] = sweep_id
+        config: typing.Dict[str, typing.Any] = run.config
+        all_configs = {
+            **config["model"],
+            **config["trainer"],
+            **config["dataset"],
+        }
+        for key in all_configs:
+            metrics[key] = all_configs[key]
+
         try:
-            dfs += [metrics.groupby("epoch").first().reset_index()]
-        except KeyError:
+            dfs += [metrics]
+        except KeyError as e:
             # this run doesn't have enough data to have 'epoch' as a key; skip for now
-            print(f"\tskipping run: {sweep_id}/{run.name}")
+            print(
+                f"\tkey `epoch` not found. skipping run: https://wandb.ai/{prefix}/{project_name}/runs/{run.name}"
+            )
             pass
 
     df = pd.concat(dfs).reset_index(drop=True)
     return df
+
+
+@typing.overload
+def get_wandb_runs(
+    project_name: str, sweep_id: str, prefix: str, samples: int
+) -> DataFrame: ...
+
+
+@typing.overload
+def get_wandb_runs(config_path: typing.Union[str, Path], samples: int) -> DataFrame: ...
+
+
+def get_wandb_runs(
+    project_name: str = None,
+    sweep_id: str = None,
+    prefix=wandbapi.viewer.username,
+    config_path: typing.Union[str, Path] = None,
+    samples=10_000,
+) -> DataFrame:
+
+    if sweep_id is None:
+        config_path = (
+            project_name  # accommodate alternate overridden function signature
+        )
+
+    if config_path is not None:
+        assert "sweep_dict" in str(config_path), (
+            f"are you sure you passed the correct config? input: {config_path}"
+        )
+        with Path(config_path).open("r") as f:
+            created_config = yaml.load(f, yaml.FullLoader)
+        for sweep in tqdm(created_config, desc="fetching sweeps from created config"):
+            # for each sweep obtain the workspace prefix and experiment name and sweep_id
+            # and fetch the runs co...rresponding to it
+            project_name = sweep["project_id"]
+            sweep_id = sweep["sweep_id"]
+            prefix = sweep["username"]
+            sweep_df = get_wandb_runs(project_name, sweep_id, prefix)
+            sweep_df_grouped_by_epoch = (
+                sweep_df.groupby(["epoch", "run_id"]).first().reset_index()
+            )
+
+            dest = Path(config_path).parent.parent / "downloaded_runs"
+            dest.mkdir(exist_ok=True)
+            sweep_df.to_csv(dest / (sweep_id + "_steps.csv"))
+            sweep_df_grouped_by_epoch.to_csv(dest / (sweep_id + "_epochs.csv"))
+
+    else:
+        return _get_wandb_runs(project_name, sweep_id, prefix)
 
 
 def _flatten_collection_of_tuples(
